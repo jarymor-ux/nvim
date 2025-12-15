65d1f7c29bef04ad484e500d3034d1e7fbf21da5
Ostap Evdokimov <evdokimov.ostap@rwb.ru>
2025-12-15 12:23:38 +0300

    wip

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -18,3 +18,10 @@ opt.swapfile = false
 opt.backup = false
 opt.writebackup = false
 
+vim.cmd('set list')
+vim.cmd('set listchars=')
+vim.cmd('set listchars+=tab:--')
+vim.cmd('set listchars+=extends:·')
+vim.cmd('set listchars+=precedes:·')
+vim.cmd('set listchars+=nbsp:·')
+vim.cmd('set listchars+=trail:·')
